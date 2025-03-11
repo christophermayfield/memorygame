632116d14f6b5d29b44ff62538c8200a17af5959
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-03-11 13:07:44 -0700

    update memory game

--- game.py
+++ game.py
@@ -13,10 +13,9 @@
 #create instance
 #call the start game method 
 
+#memory game
 class Game:
     def __init__(self):
         self.size = 4
         self.card_options = ['Add', 'Boo', 'Cat', 'Dev', 'Egg', 'Far', 'Gum', 'Hut']
         
-
-
