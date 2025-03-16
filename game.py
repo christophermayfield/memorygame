cce5feb56ff3d6526838b03ef08d5b3ae79060b7
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-03-15 17:15:41 -0700

    add check_match function

--- game.py
+++ game.py
@@ -46,12 +46,36 @@ class Game:
                     if card.matched:
                         row.append(str(card))
                     else:
-                        row.append('   ')
+                        row.append('   ') #append 3 empty spcaes if wrong
         return row
 
     def create_grid(self):
         # /  A  /  B  /  C  /  D  /
-        
+        header = ' |  '+ '  |  '.join(self.columns) + '  |'
+        print(header)
+        for row in range(1,self.size + 1):
+            print_row = f'{row}| '
+            get_row = self.create_row(row)
+            print_row+=' | '.join(get_row)+' |'
+            print(print_row)
+
+            
+
+    def check_match(self,loc1,loc2):
+        cards = []
+        for card in self.cards:
+            if card.location == loc1 or card.location == loc2:
+                cards.append(card)
+        if cards[0] == cards[1]:
+            cards[0].matched = True
+            cards[1].matched = True 
+            return True
+        else:
+            for card in cards:
+                print(f'{card.location}: {card}')
+                return False      
+
+          
          
      
         
@@ -59,13 +83,16 @@ class Game:
 if __name__== "__main__":
     mygame = Game()
     mygame.set_cards()
+
     mygame.cards[0].matched = True
     mygame.cards[1].matched = True
     mygame.cards[2].matched = True
-    mygame.cards[3].matched = True
-    print(mygame.create_row(1))
-    print(mygame.create_row(2))
-    print(mygame.create_row(3))
-    print(mygame.create_row(4))
+    mygame.cards[8].matched = True
+    mygame.create_grid()
+    #print(mygame.create_row(1))
+    #print(mygame.create_row(2))
+    #print(mygame.create_row(3))
+    #print(mygame.create_row(4))
+    
 
     
